15c1a3b77c382de1742e76ab4c6645905ca5a82f
YaroslavMod <yaroslav2211m@gmail.com>
2024-10-08 22:41:03 +0300

    update2 lab2

--- lab2/sketch.js
+++ lab2/sketch.js
@@ -1,7 +1,6 @@
 let color='#964b00';
 let k=2;
 let eye=2*k;
-let stat;
 let x;
 let y;
 let xSpecial;
@@ -42,47 +41,85 @@ function setup()
 function draw()
 {
     background(155,200,200);
-    if (keyIsPressed == false){
-        stat=1;}
-    
-    //Стоячее положение
-    if (stat==1){
+
+    //Прыжок влево
+    if (keyIsDown(87) === true &&
+        keyIsDown(65) === true &&
+        keyIsDown(68) === false){
     fill(color);
     rect(x,
          y,
-         telo.height,
+         telo.width,
          telo.height); //тело
-    rect(x,
+    rect((x-noga.height+noga.width),
          (y+telo.height),
-         noga.width,
-         noga.height); //левая нога
-    rect((x+telo.height-noga.width),
+         noga.height,
+         noga.width); //передняя нога
+    rect((x+telo.width-noga.width),
          (y+telo.height),
-         noga.width,
-         noga.height); //правая нога
-    rect((x+(telo.height*1/2)-(golova.height*1/2)),
+         noga.height,
+         noga.width); //задняя нога
+    rect((x-(golova.width*2/3)),
          (y-golova.height),
-         golova.height,golova.height); //голова
-    rect((x+(telo.height*1/2)-(golova.height*6/8)),
-         (y-golova.height),
-         (golova.height*2/8),
-         (golova.height*1/2)); //левое ухо
-    rect((x+(telo.height*1/2)+(golova.height*1/2)),
-         (y-golova.height),
-         (golova.height*2/8),
-         (golova.height*1/2)); //правое ухо
+         golova.width,
+         golova.height); //голова
+    triangle((x+(golova.width*1/3)),
+             (y-golova.height),
+             (x+(golova.width*1/6)),
+             (y-golova.height),
+             (x+(golova.width*2/9)),
+             (y-(golova.height)/2)); //ухо
+    rect((x+telo.width),
+         y,
+         hvost.height,
+         hvost.width); //хвост
     fill(0);
-    ellipse((x+(telo.height*1/2)-(golova.height*1/4)),
+    ellipse(x,
             (y-(golova.height*3/4)),
             eye,
-            eye); //левый глаз   
-    ellipse((x+(telo.height*1/2)+(golova.height*1/4)),
-            (y-(golova.height*3/4)),
-            eye,
-            eye);} //правый глаз 
+            eye);} //глаз
     
+    //Прыжок вправо
+    else if (keyIsDown(68) === true &&
+             keyIsDown(87) === true &&
+             keyIsDown(65) === false){
+    fill(color);
+    rect(xSpecial,
+         y,
+         telo.width,
+         telo.height); //тело
+    rect((xSpecial-noga.height+noga.width),
+         (y+telo.height),
+         noga.height,
+         noga.width); //задняя нога
+    rect((xSpecial+telo.width-noga.width),
+         (y+telo.height),
+         noga.height,
+         noga.width); //передняя нога
+    rect((xSpecial+telo.width-(golova.width*1/3)),
+         (y-golova.height),
+         golova.width,
+         golova.height); //голова
+    triangle((xSpecial+telo.width-(golova.width*1/3)),
+             (y-golova.height),
+             (xSpecial+telo.width-(golova.width*1/6)),
+             (y-golova.height),
+             (xSpecial+telo.width-(golova.width*2/9)),
+             (y-(golova.height)/2)); //ухо
+    rect((xSpecial-hvost.height),
+         y,
+         hvost.height,
+         hvost.width); //хвост
+    fill(0);
+    ellipse((xSpecial+telo.width),
+            (y-(golova.height*3/4)),
+            eye,
+            eye);} //глаз
+     
     //Движение вправо
-    if (stat==2){
+    else if (keyIsDown(68) === true &&
+             keyIsDown(87) === false &&
+             keyIsDown(65) === false){
     fill(color);
     rect(xSpecial,
          y,
@@ -117,7 +154,9 @@ function draw()
             eye);} //глаз
         
     //Движение влево
-    if (stat==3){
+    else if (keyIsDown(65) === true &&
+             keyIsDown(87) === false &&
+             keyIsDown(68) === false){
     fill(color);
     rect(x,
          y,
@@ -144,78 +183,11 @@ function draw()
     fill(0);
     ellipse(x,(y-(golova.height*3/4)),eye,eye);} //глаз
     
-    //Прыжок вправо
-    if (stat==5){
-    fill(color);
-    rect(xSpecial,
-         y,
-         telo.width,
-         telo.height); //тело
-    rect((xSpecial-noga.height+noga.width),
-         (y+telo.height),
-         noga.height,
-         noga.width); //задняя нога
-    rect((xSpecial+telo.width-noga.width),
-         (y+telo.height),
-         noga.height,
-         noga.width); //передняя нога
-    rect((xSpecial+telo.width-(golova.width*1/3)),
-         (y-golova.height),
-         golova.width,
-         golova.height); //голова
-    triangle((xSpecial+telo.width-(golova.width*1/3)),
-             (y-golova.height),
-             (xSpecial+telo.width-(golova.width*1/6)),
-             (y-golova.height),
-             (xSpecial+telo.width-(golova.width*2/9)),
-             (y-(golova.height)/2)); //ухо
-    rect((xSpecial-hvost.height),
-         y,
-         hvost.height,
-         hvost.width); //хвост
-    fill(0);
-    ellipse((xSpecial+telo.width),
-            (y-(golova.height*3/4)),
-            eye,
-            eye);} //глаз
-    
-    //Прыжок влево
-    if (stat==6){
-    fill(color);
-    rect(x,
-         y,
-         telo.width,
-         telo.height); //тело
-    rect((x-noga.height+noga.width),
-         (y+telo.height),
-         noga.height,
-         noga.width); //передняя нога
-    rect((x+telo.width-noga.width),
-         (y+telo.height),
-         noga.height,
-         noga.width); //задняя нога
-    rect((x-(golova.width*2/3)),
-         (y-golova.height),
-         golova.width,
-         golova.height); //голова
-    triangle((x+(golova.width*1/3)),
-             (y-golova.height),
-             (x+(golova.width*1/6)),
-             (y-golova.height),
-             (x+(golova.width*2/9)),
-             (y-(golova.height)/2)); //ухо
-    rect((x+telo.width),
-         y,
-         hvost.height,
-         hvost.width); //хвост
-    fill(0);
-    ellipse(x,
-            (y-(golova.height*3/4)),
-            eye,
-            eye);} //глаз
-
     //Прыжок на месте
-    if (stat==4){
+    else if ((keyIsDown(87) === true && 
+              keyIsDown(68) === false &&
+              keyIsDown(65) === false) ||
+              keyIsDown(87)+keyIsDown(65)+keyIsDown(68)==3){
     fill(color);
     rect(x,
          y,
@@ -233,13 +205,13 @@ function draw()
          (y-golova.height),
          golova.height,
          golova.height); //голова
-    rect((x+(telo.height*1/2)-(golova.height*6/8)),
+    rect((x+(telo.height*1/2)-(golova.height*3/4)),
          (y-golova.height),
-         (golova.height*2/8),
+         (golova.height*1/4),
          (golova.height*1/2)); //левое ухо
     rect((x+(telo.height*1/2)+(golova.height*1/2)),
          (y-golova.height),
-         (golova.height*2/8),
+         (golova.height*1/4),
          (golova.height*1/2)); //правое ухо
     fill(0);
     ellipse((x+(telo.height*1/2)-(golova.height*1/4)),
@@ -250,18 +222,41 @@ function draw()
             (y-(golova.height*3/4)),
             eye,
             eye);} //правый глаз 
-}
-
-function keyPressed()
-{
-    if (key==='d'){
-        stat=2;}
-    else if (key==='a'){
-        stat=3;}
-    else if (key==='w'){
-        stat=4;}
-    else if (key==='e'){
-        stat=5;}
-    else if (key==='q'){
-        stat=6;}   
+     
+    //Стоячее положение
+    else if (keyIsDown(87)+keyIsDown(65)+keyIsDown(68)==0 ||
+            keyIsDown(65)+keyIsDown(68)==2){
+    fill(color);
+    rect(x,
+         y,
+         telo.height,
+         telo.height); //тело
+    rect(x,
+         (y+telo.height),
+         noga.width,
+         noga.height); //левая нога
+    rect((x+telo.height-noga.width),
+         (y+telo.height),
+         noga.width,
+         noga.height); //правая нога
+    rect((x+(telo.height*1/2)-(golova.height*1/2)),
+         (y-golova.height),
+         golova.height,golova.height); //голова
+    rect((x+(telo.height*1/2)-(golova.height*3/4)),
+         (y-golova.height),
+         (golova.height*1/4),
+         (golova.height*1/2)); //левое ухо
+    rect((x+(telo.height*1/2)+(golova.height*1/2)),
+         (y-golova.height),
+         (golova.height*1/4),
+         (golova.height*1/2)); //правое ухо
+    fill(0);
+    ellipse((x+(telo.height*1/2)-(golova.height*1/4)),
+            (y-(golova.height*3/4)),
+            eye,
+            eye); //левый глаз   
+    ellipse((x+(telo.height*1/2)+(golova.height*1/4)),
+            (y-(golova.height*3/4)),
+            eye,
+            eye);} //правый глаз 
 }
